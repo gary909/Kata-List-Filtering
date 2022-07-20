cf5fb0df20b9ae93e314ad2ec5eba922ee2128c3
gary <gary.white@lodgit.com>
2022-07-20 13:00:07 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 function filter_list(l) {
-    // Return a new array with the strings filtered out
+    let myArr = [];
+    for (var i = 0; i < l.length; i++) {
+        if (typeof (l[i]) == 'number') {
+            myArr.push(l[i]);
+        }
+    }
+    return myArr;
 }
 
 console.log(filter_list([1, 2, 'a', 'b'])); // [1,2]
